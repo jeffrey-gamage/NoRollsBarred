2064abeda4222fafb04093f0883546349d6ca8b0
Alexander Gagne <schrodingerfu314@gmail.com>
2020-07-10 16:14:20 -0700

    plate+piece prefabs
    next is to add conveyor f'n to instantiate them as necessary

---
+++ NoRollsBarred/Assets/Scripts/Conveyor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyor : MonoBehaviour
+{
+    // in charge of rendering food in the correct spots and 
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ NoRollsBarred/Assets/Scripts/Plate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plate : MonoBehaviour
+{
+    // these are for checking what's full and empty.  when gridEmpty is empty, the plate is filled.
+    // when the player drops a piece, each dropped square is checked against gridFilled; if any squares overlap, the drop fails.
+    public Vector2[] gridEmpty;
+    public Vector2[] gridFilled;
+}
