f0151d1779a3e50cdf75eaf75b973a853ac3a385
Alexander Gagne <schrodingerfu314@gmail.com>
2020-07-11 11:37:37 -0700

    audioManager working
    more or less... added some sample files to test, and it looks to work fine.  we'll need to decide later when to play different tracks.

--- NoRollsBarred/Assets/Scripts/AudioManager.cs
+++ NoRollsBarred/Assets/Scripts/AudioManager.cs
@@ -4,19 +4,45 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    private static AudioManager instance;
+
     public AudioClip[] musicList;
     public AudioClip[] sfx;
 
+    private AudioSource musicPlayer;
+    private AudioSource sfxPlayer;
 
-    // Start is called before the first frame update
-    void Start()
+    void playSfx(int i)
     {
-        
+        // play the AudioClip at sfx[i]
+        sfxPlayer.clip = sfx[i];
+        sfxPlayer.Play();
+    }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        musicPlayer = this.transform.GetChild(0).GetComponent<AudioSource>();
+        sfxPlayer = this.transform.GetChild(1).GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // check which music should be playing, and play it if it's not playing
+        if (!musicPlayer.isPlaying)
+        {
+            musicPlayer.clip = musicList[1];
+            musicPlayer.Play();
+        }
+        // sfx aren't played here, they're called in a separate function from GameManager
     }
 }
