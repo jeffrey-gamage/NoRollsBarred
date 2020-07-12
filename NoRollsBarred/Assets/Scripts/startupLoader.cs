5aba432c8c16c22f08ccbe46b7eeafae59ffc961
Alexander Gagne <schrodingerfu314@gmail.com>
2020-07-11 21:25:55 -0700

    working startup screen
    even transitions smoothly to the game scene

---
+++ NoRollsBarred/Assets/Scripts/startupLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startupLoader : MonoBehaviour
+{
+    public GameObject panel;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        panel = this.transform.GetChild(3).gameObject;
+        panel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
